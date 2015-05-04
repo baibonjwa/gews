a14d0e3fbb9b8017f4fe5ca115fc9ff22c8a5d43
happybai <happybyronbai@gmail.com>
2015-05-04 15:51:10 +0800

    removed unuse nunit package

---
+++ sys5.Tests/TunnelTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using LibEntity;
+using NUnit.Framework;
+
+namespace sys5.Tests
+{
+    [TestFixture]
+    public class TunnelTests
+    {
+        [Test]
+        public void AddedTunnelUp()
+        {
+            //var tunnel = new Tunnel
+            //{
+                
+            //}
+
+        }
+        [Test]
+        public void AddedTunnelDown()
+        {
+
+
+        }
+        [Test]
+        public void AddedTunnelLeft()
+        {
+
+        }
+
+        [Test]
+        public void AddedTunnelRight()
+        {
+
+        }
+    }
+}
